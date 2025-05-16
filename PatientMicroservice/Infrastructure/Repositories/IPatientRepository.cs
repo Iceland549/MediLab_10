a524472d8e47ecdd641c4ea1468be58142e2c1f8
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-16 12:50:43 +0200

    feat(patient): add IPatientRepository interface with async CRUD signatures

--- PatientMicroservice/Infrastructure/Repositories/IPatientRepository.cs
+++ PatientMicroservice/Infrastructure/Repositories/IPatientRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace PatientMicroservice.Infrastructure.Repositories
+﻿using PatientMicroservice.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PatientMicroservice.Infrastructure.Repositories
 {
     public interface IPatientRepository
     {
+        Task<List<Patient>> GetAllAsync();
+        Task<Patient> GetByIdAsync(int id);
+        Task AddAsync(Patient patient);
+        Task UpdateAsync(Patient patient);
+        Task DeleteAsync(int id);
+
     }
 }
