b183956853af680f380cdae17b9001fafc3c4398
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-16 12:51:06 +0200

    feat(patient): add IPatientService interface for patient DTO operations

--- PatientMicroservice/Application/Interfaces/IPatientService.cs
+++ PatientMicroservice/Application/Interfaces/IPatientService.cs
@@ -1,6 +1,15 @@
-﻿namespace PatientMicroservice.Application.Interfaces
+﻿using PatientMicroservice.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PatientMicroservice.Application.Interfaces
 {
     public interface IPatientService
     {
+        Task<List<PatientDto>> GetAllPatientsAsync();
+        Task<PatientDto> GetPatientByIdAsync(int id);
+        Task AddPatientAsync(PatientDto patientDto);
+        Task UpdatePatientAsync(int id, PatientDto patientDto);
+        Task DeletePatientAsync(int id);
     }
 }
