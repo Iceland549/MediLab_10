8cc6bf894a88487091027511506f705d913ce5ce
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-16 11:09:52 +0200

    feat(patient): implement PatientDbContext with initial configuration and data seeding
    - Added PatientDbContext class inheriting from DbContext
    - Configured DbSet<Patient> and OnModelCreating override
    - Seeded initial patient data for test cases

--- PatientMicroservice/Infrastructure/Data/PatientDbContext.cs
+++ PatientMicroservice/Infrastructure/Data/PatientDbContext.cs
@@ -3,8 +3,58 @@ using PatientMicroservice.Domain.Models;
 
 namespace PatientMicroservice.Infrastructure.Data
 {
-    public class PatientDbContext
+    public class PatientDbContext : DbContext 
     {
+        public DbSet<Patient> Patients { get; set; }
+        public PatientDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>().HasKey(p => p.Id);
+
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient
+                {
+                    Id = 1,
+                    FirstName = "TestNone",
+                    LastName = "Test",
+                    DateOfBirth = new DateTime(19966, 12, 31),
+                    Gender = "F",
+                    Adress = "1 Brookside St",
+                    PhoneNumber = "100-222-3333"
+                },
+                new Patient
+                {
+                    Id = 2,
+                    FirstName = "TestBorderline",
+                    LastName = "Test",
+                    DateOfBirth = new DateTime(1945, 6, 24),
+                    Gender = "M",
+                    Adress = "2 High St",
+                    PhoneNumber = "200-333-4444"
+                },
+                new Patient
+                {
+                    Id = 3,
+                    FirstName = "TestInDanger",
+                    LastName = "Test",
+                    DateOfBirth = new DateTime(2004, 6, 18),
+                    Gender = "M",
+                    Adress = "3 Club Road",
+                    PhoneNumber = "300-444-5555"
+                },
+                new Patient
+                {
+                    Id = 4,
+                    FirstName = "TestEarlyOnSet",
+                    LastName = "Test",
+                    DateOfBirth = new DateTime(2002, 6, 28),
+                    Gender = "F",
+                    Adress = "4 Valley Dr",
+                    PhoneNumber = "400-555-6666"
+                }
+            );
+        }
     }
 }
 
