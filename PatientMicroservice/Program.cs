12e517d07b8f2ef7272570d42c5441be2e6c73a1
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-17 08:31:52 +0200

    feat: add DI, SQL Server DbContext, and JWT auth setup

--- PatientMicroservice/Program.cs
+++ PatientMicroservice/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using PatientMicroservice.Application.Interfaces;
+using PatientMicroservice.Application.Services;
+using PatientMicroservice.Infrastructure.Data;
+using PatientMicroservice.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<PatientDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IPatientService, PatientService> ();
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://mocalhost:5004";
+        //options.RequiredHttpsMetadata = false;
+        options.Audience = "patientapi";
+    });
+builder.Services.AddAuthorization();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
