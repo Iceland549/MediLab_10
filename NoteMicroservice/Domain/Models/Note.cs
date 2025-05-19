00002699f0d8d5748ce86af133ddc2780754ea64
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 09:28:46 +0200

    feat(note): add Note model for MongoDB document mapping

--- NoteMicroservice/Domain/Models/Note.cs
+++ NoteMicroservice/Domain/Models/Note.cs
@@ -1,6 +1,22 @@
-﻿namespace NoteMicroservice.Domain.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace NoteMicroservice.Domain.Models
 {
     public class Note
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("PatientId")]
+        public int PatientId { get; set; }
+
+        [BsonElement("Content")]
+        public string? Content { get; set; }
+
+        [BsonElement("DateCreated")]
+        public DateTime DateCreated { get; set; }
     }
 }
