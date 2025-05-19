53db753d5a87de69e6935bab2fe6c38cddb30a0a
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 13:37:31 +0200

    feat: configure MongoDb, dependency injection and JWT authentication in Program.cs

--- NoteMicroservice/Program.cs
+++ NoteMicroservice/Program.cs
@@ -1,8 +1,30 @@
+using Microsoft.Extensions.Options;
+using NoteMicroservice.Application.Interfaces;
+using NoteMicroservice.Application.Services;
+using NoteMicroservice.Infrastructure.Data;
+using NoteMicroservice.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbConfig>(
+    builder.Configuration.GetSection("MongoDbConfig"));
+
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<INoteService, NoteService>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:5004";
+        options.RequireHttpsMetadata = false;
+        options.Audience = "notemicroservice";
+    });
+
+builder.Services.AddAuthorization();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +40,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
