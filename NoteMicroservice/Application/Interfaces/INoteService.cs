1fc2887eb8e7272821105e1631dfb090f3a2eed0
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 12:01:14 +0200

    feat: add INoteService interface for note operations using DTOs

--- NoteMicroservice/Application/Interfaces/INoteService.cs
+++ NoteMicroservice/Application/Interfaces/INoteService.cs
@@ -1,6 +1,15 @@
-﻿namespace NoteMicroservice.Application.Interfaces
+﻿using NoteMicroservice.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NoteMicroservice.Application.Interfaces
 {
     public interface INoteService
     {
+        Task<List<NoteDto>> GetNotesByPatientIdAsync(int patientId);
+        Task<NoteDto> GetNoteByIdAsync(string id);
+        Task AddNoteAsync(NoteDto noteDto);
+        Task UpdateNoteAsync(string id, NoteDto noteDto);
+        Task DeleteNoteAsync(string id);
     }
 }
