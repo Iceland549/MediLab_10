2f531c2a37cc36191e8f0b88a361b45bcba36435
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 11:28:06 +0200

    feat: add INoteRepository interface with async CRUD methods

--- NoteMicroservice/Infrastructure/Repositories/INoteRepository.cs
+++ NoteMicroservice/Infrastructure/Repositories/INoteRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace NoteMicroservice.Infrastructure.Repositories
+﻿using NoteMicroservice.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NoteMicroservice.Infrastructure.Repositories
 {
     public interface INoteRepository
     {
+        Task<List<Note>> GetNotesByPatientIdAsync(int patientId);
+        Task<Note> GetByIdAsync(string id);
+        Task AddAsync(Note note);
+        Task UpdateAsync(Note note);
+        Task DeleteAsync(string id);
     }
 }
