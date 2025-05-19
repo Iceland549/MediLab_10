cd7e57051ab1f2f9f055b94c2d9c37077184efbd
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 09:36:23 +0200

    feat(note): add NoteDto for data transfer in NoteMicroservice

--- NoteMicroservice/Application/DTOs/NoteDto.cs
+++ NoteMicroservice/Application/DTOs/NoteDto.cs
@@ -1,6 +1,12 @@
-﻿namespace NoteMicroservice.Application.DTOs
+﻿using System;
+
+namespace NoteMicroservice.Application.DTOs
 {
     public class NoteDto
     {
+        public string Id { get; set; } = string.Empty;
+        public int PatientId { get; set; }
+        public string? Content { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
