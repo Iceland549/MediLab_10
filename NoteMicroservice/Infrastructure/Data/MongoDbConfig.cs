735738478130fc3a0f2dbd4b856be0bc52fbda5a
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-19 10:14:48 +0200

    feat(config): add MongoDbConfig class and MongoDB settings to appsettings.json

---
+++ NoteMicroservice/Infrastructure/Data/MongoDbConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace NoteMicroservice.Infrastructure.Data
+{
+    public class MongoDbConfig
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string NotesCollectionName {  get; set; } = string.Empty; 
+    }
+}
