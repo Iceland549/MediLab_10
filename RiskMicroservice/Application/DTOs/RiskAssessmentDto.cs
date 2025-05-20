239c90e662cce572b226b3528560bd5e61ef465e
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 09:17:19 +0200

    feat(risk): add RiskAssessmentDto for risk assessment API response

--- RiskMicroservice/Application/DTOs/RiskAssessmentDto.cs
+++ RiskMicroservice/Application/DTOs/RiskAssessmentDto.cs
@@ -1,6 +1,15 @@
-﻿namespace RiskMicroservice.Application.DTOs
+﻿using System.Collections.Generic;
+
+namespace RiskMicroservice.Application.DTOs
 {
     public class RiskAssessmentDto
     {
+        public int PatientId { get; set; }
+        public string PatientFirstName { get; set; } = string.Empty;
+        public string PatientLastName { get; set;} = string.Empty;
+        public int Age { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public List<string> Notes { get; set; } = new();
+        public string RiskLevel { get; set; } = string.Empty;
     }
 }
