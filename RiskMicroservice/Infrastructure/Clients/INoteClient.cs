ad620d82fab3bddb3bfad77fa1e53e37b174c035
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 10:41:53 +0200

    feat(risk): add INoteClient interface for note data retrieval

--- RiskMicroservice/Infrastructure/Clients/INoteClient.cs
+++ RiskMicroservice/Infrastructure/Clients/INoteClient.cs
@@ -1,6 +1,11 @@
-﻿namespace RiskMicroservice.Infrastructure.Clients
+﻿using RiskMicroservice.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RiskMicroservice.Infrastructure.Clients
 {
     public interface INoteClient
     {
+        Task<NoteDto?> GetNotesByPatientIdAsync(int patientId, string jwtToken);
     }
 }
