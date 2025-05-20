46ff2d2e8f4a0f0636a8d062d1837012efd78935
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 10:26:20 +0200

    feat(risk): add PatientDto and NoteDto for external data retrieval

---
+++ RiskMicroservice/Application/DTOs/NoteDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RiskMicroservice.Application.DTOs
+{
+        public class NoteDto
+        {
+            public string Id { get; set; } = string.Empty;
+            public int PatientId { get; set; }
+            public string? Content { get; set; }
+            public DateTime DateCreated { get; set; }
+        }
+}
+
+
---
+++ RiskMicroservice/Application/DTOs/PatientDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RiskMicroservice.Application.DTOs
+{
+    public class PatientDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string? Adress { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
