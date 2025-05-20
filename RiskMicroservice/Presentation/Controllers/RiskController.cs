4029bbcee5d618e6bb994a67cefed189a622237d
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 12:42:23 +0200

    feat(risk): add RiskController to expose risk assessment API endpoint

--- RiskMicroservice/Presentation/Controllers/RiskController.cs
+++ RiskMicroservice/Presentation/Controllers/RiskController.cs
@@ -1,6 +1,35 @@
-﻿namespace RiskMicroservice.Presentation.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RiskMicroservice.Application.DTOs;
+using RiskMicroservice.Application.Interfaces;
+using System.Threading.Tasks;
+
+namespace RiskMicroservice.Presentation.Controllers
 {
-    public class RiskController
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class RiskController : ControllerBase
     {
+        private readonly IRiskService _riskService;
+
+        public RiskController(IRiskService riskService)
+        {
+            _riskService = riskService;
+        }
+
+        [HttpGet("{patentId}")]
+        public async Task<IActionResult> GetRisk(int patientId)
+        {
+            var jwtToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (string.IsNullOrWhiteSpace(jwtToken))
+                return Unauthorized();
+
+            var result = await _riskService.AssessmentRiskAsync(patientId, jwtToken);
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
     }
+
 }
