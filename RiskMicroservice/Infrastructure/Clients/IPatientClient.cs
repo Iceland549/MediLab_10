7cf3595c8f69e2be8b420a47bb4ef4bb9ac85bd1
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 09:33:36 +0200

    feat(risk): add IPatientClient interface for patient data retrieval

--- RiskMicroservice/Infrastructure/Clients/IPatientClient.cs
+++ RiskMicroservice/Infrastructure/Clients/IPatientClient.cs
@@ -1,6 +1,9 @@
-﻿namespace RiskMicroservice.Infrastructure.Clients
+﻿using System.Threading.Tasks;
+
+namespace RiskMicroservice.Infrastructure.Clients
 {
     public interface IPatientClient
     {
+        Task GetPatientByIdAsync(int patientId, string jwtToken);
     }
 }
