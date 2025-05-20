e99e1621f92d4405ffd2a5257186006be62c2dc2
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-20 13:09:17 +0200

    feat(config): add DI for RiskService, PatientClient, NoteClient and configure JWT authentication

--- RiskMicroservice/Program.cs
+++ RiskMicroservice/Program.cs
@@ -1,8 +1,28 @@
+using RiskMicroservice.Application.Interfaces;
+using RiskMicroservice.Application.Services;
+using RiskMicroservice.Infrastructure.Clients;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IRiskService,RiskService>();
+builder.Services.AddScoped<IPatientClient, PatientClient>();
+builder.Services.AddScoped<INoteClient, NoteClient>();
 
 builder.Services.AddControllers();
+
+// Authentication JWT
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:5004";
+        options.RequireHttpsMetadata = false;
+        options.Audience = "riskmicroservice";
+    });
+builder.Services.AddAuthorization();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
