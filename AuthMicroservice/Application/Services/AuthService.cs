69480f22ff2936b60c7e9df81ed343041d6d56a7
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-22 09:11:52 +0200

    feat(auth): add AuthService implementation

--- AuthMicroservice/Application/Services/AuthService.cs
+++ AuthMicroservice/Application/Services/AuthService.cs
@@ -1,6 +1,65 @@
-﻿namespace AuthMicroservice.Application.Services
+﻿using AuthMicroservice.Application.DTOs;
+using AuthMicroservice.Application.Interfaces;
+using AuthMicroservice.Domain.Models;
+using AuthMicroservice.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthMicroservice.Application.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IUserRepository userRepository, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+        public async Task<string?> AuthenticateAsync(UserDto userDto)
+        {
+            var user = await _userRepository.GetByUserNameAsync(userDto.UserName); ;
+            if (user == null)
+                return null;
+
+            var isPasswordValid = await _userRepository.CheckPasswordAsync(user, userDto.Password);
+            if (!isPasswordValid) 
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var keyString = _configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(keyString))
+                throw new InvalidOperationException("JWT secret key is not configured (Jwt:Key).");
+
+            var key = Encoding.ASCII.GetBytes(keyString);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName ?? string.Empty),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
