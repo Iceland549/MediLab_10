8185a3c8ecb660302d3dfcf142a20a5b9381c692
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-22 09:12:36 +0200

    feat(auth): add AuthController for user login endpoint

--- AuthMicroservice/Presentation/Controllers/AuthController.cs
+++ AuthMicroservice/Presentation/Controllers/AuthController.cs
@@ -1,6 +1,29 @@
-﻿namespace AuthMicroservice.Presentation.Controllers
+﻿using AuthMicroservice.Application.DTOs;
+using AuthMicroservice.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthMicroservice.Presentation.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserDto userDto)
+        {
+            var token = await _authService.AuthenticateAsync(userDto);
+            if (token == null)
+                return Unauthorized(new { message = "Invalid username or password" });
+
+            return Ok(new { token });
+        }
     }
 }
