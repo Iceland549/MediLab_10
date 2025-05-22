f2177595bd69ab0ce3edbf26e5e4f69088342491
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-22 08:49:54 +0200

    feat(auth): add AuthDbContext with IdentityDbContext support

--- AuthMicroservice/Infrastructure/Data/AuthDbContext.cs
+++ AuthMicroservice/Infrastructure/Data/AuthDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace AuthMicroservice.Infrastructure.Data
+﻿using AuthMicroservice.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthMicroservice.Infrastructure.Data
 {
-    public class AuthDbContext
+    public class AuthDbContext : IdentityDbContext
     {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
     }
 }
