ea34f6aac4b0af5bb36a9c7994ef555173744a2f
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-05-22 08:55:46 +0200

    feat(auth): add IUserRepository interface

--- AuthMicroservice/Infrastructure/Repositories/IUserRepository.cs
+++ AuthMicroservice/Infrastructure/Repositories/IUserRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace AuthMicroservice.Infrastructure.Repositories
+﻿using System.Threading.Tasks;
+using AuthMicroservice.Domain.Models;
+
+namespace AuthMicroservice.Infrastructure.Repositories
 {
     public interface IUserRepository
     {
+        Task<User?> GetByUserNameAsync(string userName);
+        Task<bool> CheckPasswordAsync(User user, string password);
     }
 }
